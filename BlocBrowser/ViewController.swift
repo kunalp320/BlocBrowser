e13da99a2e74f1c7ddb5d2463cd521263c6b0d81
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-03-22 19:06:07 -0700

    Cant handle the error case

--- BlocBrowser/ViewController.swift
+++ BlocBrowser/ViewController.swift
@@ -9,31 +9,66 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate {
  
     private var webView : WKWebView!
+    private var urlTextField : UITextField?
     
     override func loadView() {
         let mainView = UIView()
-
+        self.edgesForExtendedLayout = UIRectEdge.None
+        
         self.webView = WKWebView()
         self.webView.navigationDelegate = self
         
-        let urlString = "http://www.wikipedia.org"
+        self.urlTextField = UITextField()
+        self.urlTextField?.keyboardType = UIKeyboardType.URL
+        self.urlTextField?.returnKeyType = UIReturnKeyType.Done
+        self.urlTextField?.autocapitalizationType = UITextAutocapitalizationType.None
+        self.urlTextField?.autocorrectionType = UITextAutocorrectionType.No
+        self.urlTextField?.placeholder = NSLocalizedString("Website URL", comment: "Placeholder text for web browser URL field")
+        self.urlTextField?.backgroundColor = UIColor(white: 220/255.0, alpha: 1)
+        self.urlTextField?.delegate = self
+        
+        let urlString = "https://www.wikipedia.org"
         let url = NSURL(string: urlString)
         
         let request = NSURLRequest(URL: url!)
         self.webView.loadRequest(request)
         
         
+        mainView.addSubview(self.urlTextField!)
         mainView.addSubview(self.webView)
         self.view = mainView
         
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.urlTextField?.resignFirstResponder()
+        
+        let urlString = urlTextField?.text ?? ""
+        var URL = NSURL(string: urlString)
+
+        if let _ = URL {
+        } else {
+            URL = NSURL(string: "https://\(urlString)")
+        }
+        
+        if URL != nil {
+            let request = NSURLRequest(URL: URL!)
+            self.webView.loadRequest(request)
+        }
+        return false
+    }
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        self.webView.frame = self.view.frame
+        let itemHeight : CGFloat = 50
+        let width = CGRectGetWidth(self.view.bounds)
+        let browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight
+        
+        self.urlTextField!.frame = CGRectMake(0, 0, width, itemHeight)
+        self.webView.frame = CGRectMake(0, CGRectGetMaxY((self.urlTextField?.frame)!), width, browserHeight)
     }
     
     override func viewDidLoad() {
